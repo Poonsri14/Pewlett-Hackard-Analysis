b101dce58af57e47634df3bb4ee7092327959c7d
Poonsri Singivipulya <puipui@Poonsris-MacBook-Pro.local>
2022-07-26 18:12:33 -0400

    adding code and retirement_info.csv.

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -47,3 +47,31 @@ FOREIGN KEY (emp_no) REFERENCES Salaries (emp_no),
 );     
 
 SELECT * FROM departments;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-3';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- creat data to the table to export as csv file
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
