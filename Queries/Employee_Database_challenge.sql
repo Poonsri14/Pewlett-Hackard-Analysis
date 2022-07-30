ba203c99bf097c4fb16caee3641d4f05e79c0aeb
Poonsri Singivipulya <puipui@Poonsris-MacBook-Pro.local>
2022-07-30 16:52:18 -0400

    adding screenshot.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,14 +29,33 @@ GROUP By ut.title
 ORDER BY count (ut.title)DESC;
 
 -- create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON (emp_no)e.emp_no, 
+e.first_name, 
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+tt.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles AS tt
+ON (e.emp_no = tt.emp_no)
+WHERE (de.to_date = ('9999-01-01'))
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
 
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Total of Mentorship Eligibility by title
+SELECT COUNT (me.title), me.title
+INTO mentorship_number
+FROM mentorship_eligibilty as me
+GROUP By me.title
+ORDER BY count (me.title)DESC;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+select * from mentorship_number;
+
+-- Total of Retiring Employee by Title.
+select SUM(count)
+from retiring_titles;
+
